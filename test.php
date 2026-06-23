0a5f3d75286aa353e45de75c15d52772a3d06dcf
Eliyah Eziwhuo <Eliyahe@roboco-op.org>
2026-06-23 09:22:27 +0900

    Remove temporary diagnostic test.php

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "pgsql: "      . (extension_loaded('pgsql') ? 'YES' : 'NO') . "\n";
-echo "pdo_pgsql: "  . (extension_loaded('pdo_pgsql') ? 'YES' : 'NO') . "\n";
-echo "pdo_mysql: "  . (extension_loaded('pdo_mysql') ? 'YES' : 'NO') . "\n";
-echo "pdo_sqlite: " . (extension_loaded('pdo_sqlite') ? 'YES' : 'NO') . "\n";
-echo "curl: "       . (extension_loaded('curl') ? 'YES' : 'NO') . "\n";
-echo "php: "        . PHP_VERSION . "\n";
